4f4282230f11f07fdbd704684b2150db08a5c82a
PM <121531128+26-PM@users.noreply.github.com>
2024-02-07 15:55:51 +0000

    math api

---
+++ math.java
@@ -0,0 +1,14 @@
+// import java.lang.*;
+public class math{
+    public static void main(String[]args){
+        System.out.println(Math.pow(2,2));
+        System.out.println(Math.abs(2-5));
+        System.out.println(Math.ceil(2.5));
+        System.out.println(Math.floor(2.5));
+        System.out.println(Math.sqrt(2));
+        System.out.println(Math.cbrt(2));
+        System.out.println(Math.max(5,5));
+        System.out.println(Math.min(2,3));
+        System.out.println(Math.round(5.9));
+    }
+}
