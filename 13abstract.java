f5a0db18b7e5ec63960d70bd57a455fa5992b02e
PM <121531128+26-PM@users.noreply.github.com>
2024-03-23 07:57:47 +0000

    implemented abstract class in java

---
+++ 13abstract.java
@@ -0,0 +1,17 @@
+public class abstractt{
+    public static void main(String args []){
+        Son s=new Son();
+        // prints coder
+        s.proffesion();
+    }
+}
+// Base class
+ public abstract class Parent{
+    abstract void proffesion();
+}
+// Children class - inheritence
+public class Son extends Parent{
+    void proffesion(){
+        System.out.println("I am a Coder.");
+    }
+}
